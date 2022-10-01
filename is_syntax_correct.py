888625acf8f753404f29e6aa2360d0513ee3aef8
Rahul Kumar <41460419+unitinguncle@users.noreply.github.com>
2022-10-01 10:42:56 +0530

    Create is_syntax_correct.py

---
+++ is_syntax_correct.py
@@ -0,0 +1,36 @@
+#Is Syntax Correct?
+#Write a function that detects syntax errors in a string that contains nothing but parantheses.
+
+#Sample input 1:  ((()) 
+
+#Sample output 1: False
+
+#Sample input 2:  ((((())))) 
+
+#Sample output 2: True
+
+#Sample input 3:  ))(( 
+
+#Sample output 3: False
+
+-------------------------------------
+def is_syntactically_correct(only_paran: str) -> bool:
+    ## Write Code Here
+    opening=0
+    closing=0
+    for ch in only_paran:
+      if opening>=closing:
+        if ch=='(':
+          opening+=1
+        elif ch==')':
+          closing+=1
+      else:
+        return False
+    if opening==0:
+      return False
+    if(opening==closing):
+      return True
+    else:
+      return False
+
+is_syntactically_correct("((())")
